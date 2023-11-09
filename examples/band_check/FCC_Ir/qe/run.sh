05f17795f76c42bc411c823865a246d7010e38d6
by-student-2017 <studentsctest@gmail.com>
2023-11-09 12:47:12 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ examples/band_check/FCC_Ir/qe/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+NCPU=`grep 'core id' /proc/cpuinfo | sort -u | wc -l`
+echo ${NCPU}
+MPI_PREFIX="mpirun -np ${NCPU}"
+
+cif2cell -p pwscf --pwscf-pseudo-PSLibrary-libdr='./../../../pseudo/psl100_PBE' --setup-all --k-resolution=0.20 --pwscf-spin=no --pwscf-run-type=scf -f POSCAR.cif
+${MPI_PREFIX} pw.x < POSCAR.scf.in | tee POSCAR.scf.out
+
+grep "Fermi" POSCAR.scf.out > info.dat
+
+ML=`grep -n K_POINTS POSCAR.scf.in | sed -e 's/:.*//g'`
+awk -v ML=${ML} '{if(NR<ML){print $0}}' POSCAR.scf.in > POSCAR.bands.in
+sed -i "s/scf/bands/" POSCAR.bands.in
+cat kpath_fcc.txt >> POSCAR.bands.in
+
+${MPI_PREFIX} pw.x < POSCAR.bands.in | tee POSCAR.bands.out
+
+${MPI_PREFIX} bands.x < bands.in
+
+./conv_qe_bands.sh
