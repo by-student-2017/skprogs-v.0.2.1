b7c4d8b7fe30a830eb60e08b733ad504e3ff7189
by-student-2017 <studentsctest@gmail.com>
2023-11-09 16:22:08 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/mio/run.sh
+++ examples/mio/run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-#/mnt/d/skprogs/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d C,H,O C,H,O
-
-#/mnt/d/skprogs/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Au Au
-
-#/mnt/d/skprogs/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Fe Fe
-
-#/mnt/d/skprogs/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Co Co
-
-/mnt/d/skprogs/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Nb Nb
+python3 /mnt/d/skprogs-v.0.2.1/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Nb Nb
