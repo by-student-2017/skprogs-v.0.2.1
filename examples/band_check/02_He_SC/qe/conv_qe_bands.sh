0d4ff76d1161666e300af3f284e039631a2ca9ab
by-student-2017 <studentsctest@gmail.com>
2023-11-15 14:44:06 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ examples/band_check/02_He_SC/qe/conv_qe_bands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+EF=`awk '{printf "%f",$5}' info.dat`
+echo "Fermi energy = ${EF} [eV] from info.dat"
+
+ytop=-30
+ybottom=30
+
+awk -v EF=${EF} -v ytop=${ytop} -v ybottom=${ybottom} '
+  BEGIN {nL=0; flag=0}
+  {
+    if( $2=="" ){ nL=NR }
+    if( $2=="" && flag==1 ){ print $0 }
+    if( (EF-$2)<ybottom && (EF-$2)>ytop && (NR-nL)>=1){
+      printf "%d %f \n",(NR-nL),(EF-$2)
+      flag=1
+    }
+  }
+  END {}' POSCAR.bands.gnu > qe_bands.dat
+
+#cat qe_bands.dat
+
