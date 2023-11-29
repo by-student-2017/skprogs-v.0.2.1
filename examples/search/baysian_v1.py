a59e7ee075747ac4fbca8eca22c2210b874d9c2c
by-student-2017 <studentsctest@gmail.com>
2023-11-29 12:03:42 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/search/baysian_v1.py
+++ examples/search/baysian_v1.py
@@ -400,13 +400,13 @@ if os.path.exists("./logs.json"):
   load_logs(new_optimizer, logs=["./logs.json"]);
   logger = JSONLogger(path="./logs", reset=False) # Results will be saved in ./logs.json
   new_optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-  new_optimizer.maximize(init_points=2, n_iter=500)
+  new_optimizer.maximize(init_points=0, n_iter=500)
   new_optimizer.set_gp_params(alpha=1e-3) # The greater the whitenoise, the greater alpha value.
 else:
   optimizer = BayesianOptimization(f=descripter, pbounds=pbounds, verbose=2, random_state=1, bounds_transformer=bounds_transformer)
   logger = JSONLogger(path="./logs") # Results will be saved in ./logs.json
   optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-  optimizer.maximize(init_points=2, n_iter=500)
+  optimizer.maximize(init_points=2, n_iter=500) # 500 cycle / 12 h
   optimizer.set_gp_params(alpha=1e-3) # The greater the whitenoise, the greater alpha value.
 #--------------------------------------------------------
 #------------------ for ucb -----------------------------
