b5b323058e3d9d0ec2009790fffd53b2a636aac8
by-student-2017 <studentsctest@gmail.com>
2023-12-01 11:03:25 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/search/pso_v1.py
+++ examples/search/pso_v1.py
@@ -399,7 +399,7 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16):
       +", "+sx14+", "+sx15+", "+sx16 # Slater-Type Orbitals of D
       +", "+str(sdtv)+", "+str(sdtv3kbt) # Evaluate values
       +" >> Evalute.txt", shell=True)
-    y[i] = etav
+    y[i] = "{:.6f}".format(etav)
 
   return y
 #----------------------------------------------------------------------
