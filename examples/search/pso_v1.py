8b21543fa775950732bcb6bde73d7ba4ad00ff30
by-student-2017 <studentsctest@gmail.com>
2023-12-01 11:40:53 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/search/pso_v1.py
+++ examples/search/pso_v1.py
@@ -57,8 +57,8 @@ ylastd = atomic_number*3.0  # D orbitals, TM: x2.0
 
 #------------------------------------------------
 # Note: Empirically, setting a value around 0.3 will significantly reduce the number of failures.
-hwb  =  0.37 # search range [-x*hwb:+x*hwt]
-hwt  =  0.37 # search range [-x*hwb:+x*hwt]
+hwb  =  0.1 # search range [-x*hwb:+x*hwt]
+hwt  =  0.1 # search range [-x*hwb:+x*hwt]
 #---------------------------
 # Note
 # 1. A value around sigma = 7.0 is often good.
@@ -399,7 +399,7 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16):
       +", "+sx14+", "+sx15+", "+sx16 # Slater-Type Orbitals of D
       +", "+str(sdtv)+", "+str(sdtv3kbt) # Evaluate values
       +" >> Evalute.txt", shell=True)
-    y[i] = "{:.6f}".format(etav)
+    y[i:i] = etav
 
   return y
 #----------------------------------------------------------------------
