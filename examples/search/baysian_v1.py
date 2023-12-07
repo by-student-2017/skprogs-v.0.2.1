717d71a1a8e1290bfb7c4f6f0ff0949c900d89a6
by-student-2017 <studentsctest@gmail.com>
2023-12-07 11:06:43 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/search/baysian_v1.py
+++ examples/search/baysian_v1.py
@@ -63,8 +63,8 @@ ylastd = atomic_number*3.0  # D orbitals, TM: x2.0
 
 #------------------------------------------------
 # Note: Empirically, setting a value around 0.3 will significantly reduce the number of failures.
-hwb  =  0.9 # search range [-x*hwb:+x*hwt]
-hwt  =  0.9 # search range [-x*hwb:+x*hwt]
+hwb  =  0.37 # search range [-x*hwb:+x*hwt]
+hwt  =  0.37 # search range [-x*hwb:+x*hwt]
 #---------------------------
 # Note
 # 1. A value around sigma = 7.0 is often good.
@@ -262,7 +262,7 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16):
   #-------------------------------
   # I realized later that {:.f} rounds it up. On the other hand, int() is truncated.
   #-------------------------------
-  fine_step = "yes"
+  fine_step = "no"
   if fine_step == "no": # 0.2 and 0.02 step for sigma and sto, respectively.
     print("0.2 and 0.02 step for sigma and sto, respectively.")
     #-------------------------------
