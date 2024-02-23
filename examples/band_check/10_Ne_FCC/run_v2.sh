357df2b1e2824e1868d99d4f643713a5e83a1e8a
by-student-2017 <studentsctest@gmail.com>
2024-02-23 14:04:04 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/band_check/10_Ne_FCC/run_v2.sh
+++ examples/band_check/10_Ne_FCC/run_v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sym=FCC # e.g., FCC, BCC, HCP, SC
+#sym=FCC # e.g., FCC, BCC, HCP, SC
 
 export OMP_NUM_THREADS=1
 #NCPU=`grep 'core id' /proc/cpuinfo | sort -u | wc -l`
@@ -19,11 +19,17 @@ echo "---------- Band calculation ----------"
 cp dftb_in_band.hsd dftb_in.hsd
 ${MPI_PREFIX} dftb+ < dftb_in.hsd > dftb_out_band.hsd
 
+echo "---------- Get Fermi level ----------"
+EF1=`awk '{if($1=="Fermi"){printf "%f",$5}}' info.dat`
+EF2=`awk 'BEGIN{EF=-30}{if($1!="KPT" && $2>EF && $3>0.0){EF=$2}}END{printf "%f",EF}' band.out`
+echo ${EF1} ${EF2} | awk '{if( (($1-$2)^2)^0.5 >= 0.003 ){printf "Fermi level: %f H %f eV from band.out file \n",$2/(13.602*2),$2}}' > info.dat
+
 echo "---------- plot (gnuplot) ----------"
 grep -v "KPT" band.out > band.dat
 #gnuplot band_${sym}.gp
 ./conv_dftbp_band.sh
-gnuplot < comp_band_${sym}.gpl
+#gnuplot < comp_band_${sym}.gpl
+gnuplot < comp_band_*.gpl
 
 echo "---------- Evaluate DS ----------"
 ./msd.sh
@@ -42,5 +48,6 @@ rm -f band.dat
 rm -f info_gnu.dat
 rm -f band.plot
 rm -f msd_band.dat
+
 
 echo "---------- Ende ----------"
