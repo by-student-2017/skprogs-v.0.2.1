806d4a7026f387ada947536a17008be04bffe6f3
by-student-2017 <studentsctest@gmail.com>
2024-03-06 16:50:31 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/band_check/55_Cs_BCC/msd.sh
+++ examples/band_check/55_Cs_BCC/msd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # These are related with comp_band.png
-ytop=-5    # Binding energy [eV], conduction band (unoccupied states)
+ytop=-3    # Binding energy [eV], conduction band (unoccupied states)
            # Fermi energy, EF = 0 [eV]
-ybottom=4  # Binding energy [eV], valence band (occupied states)
+ybottom=3  # Binding energy [eV], valence band (occupied states)
 
 echo "---------------------------------"
 if [ ! -e qe_bands.dat ]; then
