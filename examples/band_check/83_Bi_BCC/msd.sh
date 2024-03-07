dfefdeade874b3f4ad2b209951c1f68219adc7f2
by-student-2017 <studentsctest@gmail.com>
2024-03-07 17:33:52 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- examples/band_check/83_Bi_BCC/msd.sh
+++ examples/band_check/83_Bi_BCC/msd.sh
@@ -38,7 +38,7 @@ kbT3=`echo ${kbT} | awk '{printf "%f",(3.0*$1)}'`
 echo "3*kbT = "${kbT3}" [eV] at ${Temp} [K]"
 
 awk -v ybottom=${ybottom} -v ytop=${ytop} -v kbT=${kbT} '
-BEGIN{n=0;VD=0.0;VDT=0.0;eta=0.0}
+BEGIN{n=0;VD=0.0;VDT=0.0;ETA=0.0}
 {
   if( (ybottom>=$3 && $3>=ytop) || (0>=$2 && $2>=ytop) ){
   #if(ybottom>=$3 && $3>=ytop){
