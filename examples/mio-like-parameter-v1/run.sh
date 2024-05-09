519e1091f35ac522ad4e60bcaf9d565bab1a1040
by-student-2017 <studentsctest@gmail.com>
2024-05-09 11:09:38 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ examples/mio-like-parameter-v1/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#python3 /mnt/d/skprogs-v.0.2.1/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Fe,Nb,Si,B Fe,Nb,Si,B
+python3 /mnt/d/skprogs-v.0.2.1/sktools/src/sktools/scripts/skgen.py -o slateratom -t sktwocnt sktable -d Nb Nb
