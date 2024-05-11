beb4c9a68c09f5f0e42560a0fb6bfb633542601b
by-student-2017 <studentsctest@gmail.com>
2024-05-11 21:34:03 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ examples/band_check/33_As_FCC/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sym=FCC # e.g., FCC, BCC, HCP, SC
+
+export OMP_NUM_THREADS=1
+
+echo "---------- SCF calculation ----------"
+cp dftb_in_scf.hsd dftb_in.hsd
+mpirun -np 1 dftb+ < dftb_in.hsd > dftb_out_scf.hsd
+
+echo "---------- Get Fermi level ----------"
+grep "Fermi level:"  < detailed.out >  info.dat
+grep "Total energy:" < detailed.out >> info.dat
+
+echo "---------- Band calculation ----------"
+cp dftb_in_band.hsd dftb_in.hsd
+mpirun -np 1 dftb+ < dftb_in.hsd > dftb_out_band.hsd
+
+echo "---------- plot (gnuplot) ----------"
+grep -v "KPT" band.out > band.dat
+#gnuplot band_${sym}.gp
+./conv_dftbp_band.sh
+gnuplot < comp_band_${sym}.gpl
+
+echo "---------- Evaluate DS ----------"
+./msd.sh
+
+echo "---------- Delete files ----------"
+rm -f dftb_in.hsd
+rm -f dftb_out_scf.hsd
+rm -f dftb_out_band.hsd
+rm -f dftb_pin.hsd
+rm -f charges.bin
+rm -f detailed.out
+#-----------------
+rm -f info.dat
+rm -f band.out
+rm -f band.dat
+rm -f info_gnu.dat
+rm -f band.plot
+rm -f msd_band.dat
+
+echo "---------- Ende ----------"
