835ba8b2b776f8f9da6360410ee9ba233eb796c8
by-student-2017 <studentsctest@gmail.com>
2024-05-12 18:04:59 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ examples/band_check/54_Xe_BCC/run_v2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sym=BCC # e.g., FCC, BCC, HCP, SC
+
+export OMP_NUM_THREADS=1
+#NCPU=`grep 'core id' /proc/cpuinfo | sort -u | wc -l`
+#echo "Number of CPU: "${NCPU}
+MPI_PREFIX="mpirun -np 1"
+
+echo "---------- SCF calculation ----------"
+cp dftb_in_scf.hsd dftb_in.hsd
+${MPI_PREFIX} dftb+ < dftb_in.hsd > dftb_out_scf.hsd
+
+echo "---------- Get Fermi level ----------"
+#grep "Fermi level:"  < detailed.out >  info.dat
+#grep "Total energy:" < detailed.out >> info.dat
+
+echo "---------- Band calculation ----------"
+cp dftb_in_band.hsd dftb_in.hsd
+${MPI_PREFIX} dftb+ < dftb_in.hsd > dftb_out_band.hsd
+
+echo "---------- plot (gnuplot) ----------"
+grep -v "KPT" band.out > band.dat
+#gnuplot band_${sym}.gp
+#./conv_dftbp_band.sh
+./conv_dftbp_band_v2.sh
+gnuplot < comp_band_${sym}.gpl
+
+echo "---------- Evaluate DS ----------"
+./msd.sh
+
+echo "---------- Delete files ----------"
+rm -f dftb_in.hsd
+rm -f dftb_out_scf.hsd
+rm -f dftb_out_band.hsd
+rm -f dftb_pin.hsd
+rm -f charges.bin
+rm -f detailed.out
+#-----------------
+rm -f info.dat
+rm -f band.out
+rm -f band.dat
+rm -f info_gnu.dat
+rm -f band.plot
+rm -f msd_band.dat
+
+echo "---------- Ende ----------"
